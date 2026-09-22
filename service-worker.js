703625fe8fd9cb2478eeaa54060716be1aec1c0c
hamadakimo89-debug <hamadakimo89@gmail.com>
2026-09-22 11:43:15 +0300

    تحديث التخزين المؤقت التلقائي لإصدار رُقي v5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ruqy-v3';
+const CACHE_NAME = 'ruqy-v5';
 
 const APP_SHELL = [
   './',
@@ -10,58 +10,37 @@ const APP_SHELL = [
   './icon-512.png'
 ];
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(APP_SHELL);
-    })
-  );
-
+self.addEventListener('install', (event) => {
   self.skipWaiting();
-});
-
-self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(
-        keys
-          .filter(function (key) {
-            return key !== CACHE_NAME;
-          })
-          .map(function (key) {
-            return caches.delete(key);
-          })
-      );
-    }).then(function () {
-      return self.clients.claim();
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
 });
 
-self.addEventListener('fetch', function (event) {
-  if (event.request.method !== 'GET') return;
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
 
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     fetch(event.request)
-      .then(function (networkResponse) {
-        if (
-          networkResponse &&
-          networkResponse.status === 200 &&
-          networkResponse.type === 'basic'
-        ) {
-          const responseCopy = networkResponse.clone();
-
-          caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request, responseCopy);
-          });
+      .then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         }
-
-        return networkResponse;
+        return response;
       })
-      .catch(function () {
-        return caches.match(event.request).then(function (cachedResponse) {
-          return cachedResponse || caches.match('./index.html');
-        });
-      })
+      .catch(() => caches.match(event.request)
+        .then((cached) => cached || caches.match('./index.html')))
   );
 });
